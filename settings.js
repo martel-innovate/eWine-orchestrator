bd9d286bc38bec5a7315436338353c6e46625c02
Tomas <taliaga@users.noreply.github.com>
2018-01-19 12:58:14 +0100

    D2df (#4)
    * Merged content of D2D branch but preserving file /lib/flows/orchestrator.json and a single settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ module.exports = {
     //debugUseColors: true,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    flowFile: 'lib/flows/orchestrator.json',
+    flowFile: 'lib/flows/orchestrator-D2D.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
