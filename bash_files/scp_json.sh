f22fe9ec55ce5ac8a5ef5f9906f747e1c6a195c6
Francesco Verde <francesco.verde@martel-innovate.com>
2018-04-06 10:16:59 +0200

    added wi-fi connectivity

--- bash_files/scp_json.sh
+++ bash_files/scp_json.sh
@@ -10,7 +10,11 @@ cp /home/pi/Downloads/temperature*.png /home/pi/eWine-orchestrator/temperature_f
 rsync -aP /home/pi/eWine-orchestrator/temperature_files/ /home/pi/Scampi\ App\ Developer\ Package\ 0.4.1b/bin/java/FileTransfer/files/temperature_images
 
 #copy humidity images to the other RPI
-scp -i /home/pi/.ssh/id_rsa /home/pi/Downloads/humidity*.png pi@192.168.2.20:/home/pi/Downloads/humidity_images/
+#ethernet rpi_y_w
+#scp -i /home/pi/.ssh/id_rsa /home/pi/Downloads/humidity*.png pi@192.168.2.20:/home/pi/Downloads/humidity_images/
+
+#wi-fi rpi_y_w
+scp -i /home/pi/.ssh/id_rsa /home/pi/Downloads/humidity*.png pi@192.168.1.217:/home/pi/Downloads/humidity_images/
 
 #remove humidity files
 #rm -rf /home/pi/Downloads/humidity*.png
